ba9072238cb36131e4bd9d0079f9315c3d9555f1
MrFreex <filippolissandrin@gmail.com>
2022-04-09 11:32:00 +0200

    chores (adjusting): change project structure
    + base content
    + removed test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import styled from 'styled-components'
+import style from './css/Style.module.css'
+import Button from './components/Button'
+import './global.css';
+import axios from 'axios'
 
-interface SectionProps {
-  background: string
-}
-
-const Section = styled.section<SectionProps>`
-  background-color: ${props => props.background};
+const Container = styled.div`
+  background-color: #000;
   padding: 3em;
 `
 
-const Section2 = styled.section`
-  background-color: blue;
-  padding: 3em;
+const Body = styled.body`
+  background: rgb(29, 29, 29);
+  padding: 0;
+  margin: 0;
 `
 
-const H1Red = styled.h1`
-  font-size: 5em;
-  color: red;
-`
-
-const Title = styled.h1`
-  font-family: 'Courier New', Courier, monospace;
-  font-size: 50px;
-  color: darkblue;
-  margin: 0px;
-`
-
-const Content = styled.h3`
-  font-family: 'Courier New', Courier, monospace;
-  font-size: 25px;
-  color: blueviolet;
-  margin: 0px 0px 10px;
-`
-
-const Input = styled.input`
-  background-color: lightcyan;
-  border: solid 4px darkblue;
-`
-
-const Button = styled.button`
-  color: darkblue;
-  border: solid 4px;
-  background-color: lightcyan;
+const Header = styled.div`
+  backdrop-filter: saturate(180%) blur(20px);
+  background:rgba(94, 94, 94, 0.5);
+  padding:10px 0px;
+  font-size:52px;
+  line-height:auto;
+  color:#012D54;
+  text-shadow: 2px 2px white;
+  border-bottom:1px solid rgba(255, 255, 255, 0.5);
+  position: absolute;
+  width: 100vw;
+  height: 5vh;
 `
 
 function App() {
   const [text, setText] = useState("");
-  
 
+  useEffect(() => {
+
+  }); 
+  
   return (
-    <Section background="cornflowerblue">
-      <Title> This is a title ✨</Title>
-      <Content>{text}</Content>
-      <Input type={"text"} onChange={(e) => setText(e.target.value)}></Input>
-      <Button onClick={() => alert("CIAO BELLOOOOOO")}>Ok</Button>
-      <Section2>
-        <H1Red>CIAO</H1Red>
-      </Section2>
-    </Section>
+    <Header className={style.Header}>
+       {/** <Container>
+        
+       </Container> */}
+       <Body></Body>
+
+    </Header>
   );
 }
 
